0b9ded0eb8abc4bb22f547e6d68d563fb44560d8
Josh Soref <jsoref@users.noreply.github.com>
2022-05-03 03:16:38 -0400

    spelling: dimensional
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- examples/statistics/confidence_ellipse.py
+++ examples/statistics/confidence_ellipse.py
@@ -67,7 +67,7 @@ def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
     cov = np.cov(x, y)
     pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
     # Using a special case to obtain the eigenvalues of this
-    # two-dimensionl dataset.
+    # two-dimensional dataset.
     ell_radius_x = np.sqrt(1 + pearson)
     ell_radius_y = np.sqrt(1 - pearson)
     ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
@@ -97,7 +97,7 @@ def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
 # A helper function to create a correlated dataset
 # """"""""""""""""""""""""""""""""""""""""""""""""
 #
-# Creates a random two-dimesional dataset with the specified
+# Creates a random two-dimensional dataset with the specified
 # two-dimensional mean (mu) and dimensions (scale).
 # The correlation can be controlled by the param 'dependency',
 # a 2x2 matrix.
